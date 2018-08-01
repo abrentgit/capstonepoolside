d902d09f31c009fcb6cfb6c7bf9a1d039269ade0
Anthony <32938216+abrentgit@users.noreply.github.com>
2018-08-01 01:38:06 -0400

    server change

--- server.js
+++ server.js
@@ -1,9 +1,18 @@
-'use strict';
+"use strict";
 
-const express = require('express');
+const express = require("express");
+
 const app = express();
-app.use(express.static('public'));
-app.listen(process.env.PORT || 8080);
+
+app.use(express.static("public"));
+
+if (require.main === module) {
+  app.listen(process.env.PORT || 8080, function() {
+    console.info(`App listening on ${this.address().port}`);
+  });
+}
+
+module.exports = app;
 
 // const morgan = require('morgan');
 // const mongoose = require('mongoose');
