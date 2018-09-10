75b5287ceaf89323d6fb8a7feaf6f45650c4f43d
Anthony <32938216+abrentgit@users.noreply.github.com>
2018-09-09 21:03:52 -0400

    initial commit

---
+++ strategies.js
@@ -0,0 +1,19 @@
+'use strict';
+
+const { JwtStrategy } = require('passport-jwt').Strategy;
+const { ExtractJwt } = require('passport-jwt').ExtractJwt;
+
+const { JWT_SECRET } = require('./config');
+
+const jwtStrategy = new JwtStrategy({
+    secretOrKey: JWT_SECRET,
+    // Look for the JWT as a Bearer auth header
+    jwtFromRequest: ExtractJwt.fromAuthHeaderWithScheme("Bearer"),
+    algorithms: ['HS256']
+  },
+  (payload, done) => {
+    done(null, payload.guest);
+  }
+);
+
+module.exports = { jwtStrategy };
