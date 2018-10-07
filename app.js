00cbbc213682669f708f0eaf057722033ecef22e
Anthony <32938216+abrentgit@users.noreply.github.com>
2018-10-07 10:41:58 -0400

    updated app.js login

--- app.js
+++ app.js
@@ -23,18 +23,23 @@ function performLogin() {
             'email': `${email}`,
             'password': `${password}`,
         };
+
+        const headers = {
+            'Content-Type': 'application/json'
+        };
     
         return fetch('http://localhost:8080/login', { 
             method: 'POST',
-            session: session
+            body: JSON.stringify(session),
+            headers: headers
         }).then(rawResponse => {
             return rawResponse.json(); 
         }).then(response => {
             console.log('request worked', response);
             return response;
-        }).then(token => {
-            const token = localStorage.getItem('token');
-            // SET TO TOKEN TO SESSION? 
+        // }).then(token => {
+        //     const token = localStorage.getItem('token');
+        //     // SET TO TOKEN TO SESSION? 
         }).catch(error => {
             console.log('an error occured', error);
         });
