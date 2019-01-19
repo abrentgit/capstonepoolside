58e0c80add2993b6f78c87b6ee324dd78961882b
abrentgit <abprojects.ca@gmail.com>
2019-01-18 21:22:01 -0500

    added db

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 
 exports.DATABASE_URL = process.env.DATABASE_URL ||
-global.DATABASE_URL || 'mongodb://orderinn_user:labrea99@ds243325.mlab.com:43325/order_inn_production_db';
+global.DATABASE_URL || 'mongodb://user:user123@ds243325.mlab.com:43325/order_inn_production_db';
 
 exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL ||'mongodb://kobefed5:jordan6@ds111063.mlab.com:11063/mock_orders';
 exports.PORT = process.env.PORT || 8080;
