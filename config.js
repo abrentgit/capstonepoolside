5ca216e6c24a6d9f0ee8f5052f7f0bd24f513ea5
abrentgit <abprojects.ca@gmail.com>
2019-01-18 21:44:30 -0500

    trying new config for heroku endpoints

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
 
-exports.DATABASE_URL = process.env.DATABASE_URL ||
+export const DATABASE_URL = process.env.DATABASE_URL ||
 global.DATABASE_URL || 'mongodb://user:user123@ds243325.mlab.com:43325/order_inn_production_db';
 
-exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL ||'mongodb://kobefed5:jordan6@ds111063.mlab.com:11063/mock_orders';
-exports.PORT = process.env.PORT || 8080;
+export const TEST_DATABASE_URL = process.env.TEST_DATABASE_URL ||'mongodb://kobefed5:jordan6@ds111063.mlab.com:11063/mock_orders';
+export const PORT = process.env.PORT || 8080;
 
-exports.JWT_SECRET = process.env.JWT_SECRET || 'kobeFed5'
-exports.JWT_EXPIRY = process.env.JWT_EXPIRY || '7d';
+export const JWT_SECRET = process.env.JWT_SECRET || 'kobeFed5'
+export const JWT_EXPIRY = process.env.JWT_EXPIRY || '7d';
